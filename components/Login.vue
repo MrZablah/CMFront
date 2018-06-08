569c238fda42b1eda74a795760ccf481ec7b39ac
Zablah <miguel12979@gmail.com>
2018-06-07 19:31:38 -0500

    basic navbar with a login modal added

---
+++ components/Login.vue
@@ -0,0 +1,67 @@
+<template>
+    <b-form @submit.prevent="onSubmit" @reset.prevent="onReset">
+        <b-form-group label="Username:" label-for="username">
+            <b-form-input id="username"
+                        type="text"
+                        v-model="form.user"
+                        required
+                        :state="valEmail"
+                        placeholder="Enter username">
+            </b-form-input>
+            <b-form-invalid-feedback id="username">
+                <!-- This will only be shown if the preceeding input has an invalid state -->
+                Enter a valid email.
+            </b-form-invalid-feedback>
+        </b-form-group>
+        <b-form-group label="Password:" label-for="password">
+            <b-form-input id="password"
+                        type="password"
+                        v-model="form.pass"
+                        :state="valUser"
+                        required
+                        placeholder="Enter password">
+            </b-form-input>
+            <b-form-invalid-feedback id="password">
+                <!-- This will only be shown if the preceeding input has an invalid state -->
+                Enter a valid password.
+            </b-form-invalid-feedback>
+        </b-form-group>
+        
+        <div class="float-right">
+            <b-button type="reset" variant="danger" class="mr-2" v-if="showReset">Reset</b-button>
+            <b-button type="submit" variant="primary">Submit</b-button>
+        </div>
+    </b-form>
+</template>
+
+<script>
+export default {
+    props: {
+        showReset: {
+            type: Boolean,
+            default: true 
+        },
+    },
+    data() {
+        return {
+            form: {
+                user: '',
+                pass: ''
+            },
+            valEmail: null,
+            valUser: null
+        }
+    },
+    methods: {
+        onSubmit (evt) {
+            console.log(JSON.stringify(this.form));
+        },
+        onReset (evt) {
+            /* Reset our form values */
+            this.form.user = '';
+            this.form.pass = '';
+        }
+    }
+}
+</script>
+
---
+++ components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="/">CM</b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+            <b-navbar-nav v-if="isLogin">
+                <b-nav-item href="#">Link</b-nav-item>
+                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            </b-navbar-nav>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                
+                <b-nav-form v-if="isLogin">
+                    <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+                    <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-nav-form>
+
+                <b-nav-item-dropdown id="drop" text="Lang" right v-if="isLogin">
+                    <b-dropdown-item href="#">EN</b-dropdown-item>
+                    <b-dropdown-item href="#">ES</b-dropdown-item>
+                </b-nav-item-dropdown>
+
+                <b-nav-item-dropdown right v-if="isLogin">
+                    <!-- Using button-content slot -->
+                    <template slot="button-content">
+                    <em>User</em>
+                    </template>
+                    <b-dropdown-item href="#">Profile</b-dropdown-item>
+                    <b-dropdown-item href="#">Signout</b-dropdown-item>
+                </b-nav-item-dropdown>
+
+                <b-btn size="sm" class="my-2 my-sm-0 ml-2" variant="success" v-b-modal.modal-center v-if="!isLogin">Login</b-btn>
+                <!-- Modal Component -->
+                <b-modal id="modal-center" centered title="Login" :hide-footer="true">
+                    <Login :showReset="false"/>
+                </b-modal>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+import Login from '~/components/Login';
+export default {
+    data() {
+        return {
+            isLogin: false
+        }
+    },
+    methods:{
+        
+    },
+    components: {
+        Login,
+    }
+}
+</script>
